985e7639a218b0f6b4843d58642db594c650bf6a
markw53 <m.workman@outlook.com>
2025-03-07 12:25:25 +0000

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// src/App.jsx
+
+import React from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
+
+// Layout
 import MainLayout from './components/layout/MainLayout';
+
+// Pages
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
 
-function App() {
+const App = () => {
+  const location = useLocation();
+
   return (
-    <Router>
-      <MainLayout>
-        <AnimatePresence mode='wait'>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </AnimatePresence>
-      </MainLayout>
-    </Router>
+    <MainLayout>
+      <AnimatePresence mode='wait'>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </AnimatePresence>
+    </MainLayout>
   );
-}
+};
 
 export default App;
